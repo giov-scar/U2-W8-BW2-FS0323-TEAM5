e50bca2f3dce6cd904770bce21ae095ae7be90bf
LorenzoAluigi <134380077+LorenzoAluigi@users.noreply.github.com>
2023-07-10 12:55:38 +0200

    search bar fetch ok in console log

--- script/index-script.js
+++ script/index-script.js
@@ -1,4 +1,39 @@
 console.log("hello world");
 
+//  URL API
 const artistUrl = "https://striveschool-api.herokuapp.com/api/deezer/artist";
 const albumUrl = "https://striveschool-api.herokuapp.com/api/deezer/album";
+const searchUrl = "https://striveschool-api.herokuapp.com/api/deezer/search?q=";
+//function to hide main
+const hideMain = function () {
+  const main = document.getElementsByTagName("main");
+  main.classlist.add("d-none");
+};
+
+// function for fetch
+
+const mysearch = function () {
+  const searchInput = document.getElementById("search");
+  let serchValue = searchInput.value;
+  fetch(`${searchUrl}${serchValue}`)
+    .then((res) => {
+      if (res.ok) {
+        console.log(res);
+        return res.json();
+      } else {
+        throw new error("error");
+      }
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const searchbutton = document.getElementById("search-button");
+searchbutton.addEventListener("click", function (e) {
+  e.preventDefault();
+  mysearch();
+});
