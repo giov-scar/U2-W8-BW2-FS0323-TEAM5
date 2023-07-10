cda8e54bb3697a58649f3e723c443b6a5a07b6c5
LorenzoAluigi <134380077+LorenzoAluigi@users.noreply.github.com>
2023-07-10 12:02:42 +0200

    add const url for fetch

--- script/index-script.js
+++ script/index-script.js
@@ -1,1 +1,4 @@
 console.log("hello world");
+
+const artistUrl = "https://striveschool-api.herokuapp.com/api/deezer/artist";
+const albumUrl = "https://striveschool-api.herokuapp.com/api/deezer/album";
