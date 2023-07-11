1791cff73ccfd3a94d25718c2ea09c73f2e75630
LorenzoAluigi <134380077+LorenzoAluigi@users.noreply.github.com>
2023-07-11 14:38:08 +0200

    track list

--- script/album-script.js
+++ script/album-script.js
@@ -10,5 +10,22 @@ const albumId = addressBarContent.get("id");
 console.log(albumId);
 
 if (albumId) {
-  fetch(url + albumId);
+  fetch(albumUrl + albumId)
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw new error(error);
+      }
+    })
+    .then((data) => {
+      let tracklist = data.tracks.data;
+      console.log(data);
+      console.log("url tracklist", tracklist);
+
+      tracklist.forEach((e) => {});
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
